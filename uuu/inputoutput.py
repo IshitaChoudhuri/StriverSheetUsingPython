904ab3f23ef7ba8c1463dbac5e5e98e7087dc030
Sushi2021 <72.probal@gmail.com>
2024-03-21 01:34:33 +0530

    commit

---
+++ uuu/inputoutput.py
@@ -0,0 +1,27 @@
+# print("Hello")
+
+# x=5
+# print(x)
+
+# y=8
+# print(x,y,"X and Y vales are printed")
+
+# #diff line
+# print(x)
+# print(y)
+
+# #same line
+# print(x,y)
+
+# print(x,"Value of x", end="-")
+# print(y)
+
+# name = input()
+# print(name, "is a good girl")
+
+# value=int(input())
+# print("age of",name,"is",value)
+
+lst = list(map(int, input().split(" ")))
+print(lst)
+print(len(lst))
