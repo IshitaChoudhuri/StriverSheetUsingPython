c11cef64dce73adec8fc7f6306ada80f7e551cd4
Sushi2021 <72.probal@gmail.com>
2024-03-21 01:46:29 +0530

    commit

---
+++ uuu/datatypes.py
@@ -0,0 +1,23 @@
+name = "ishita choudhuri"  # string
+age = 18  # integer
+gender = "F"  # string
+weight = 54.5  # float
+isStudent = True  # boolean
+fruits = {"apple", "banana", "cherry"}  # set
+earning = [22, 21, 33, 44, 22, 21]  # list
+earn = (21, 22, 22, 71, 67)  # tuple
+info = {"name": "Ishita Choudhuri", "age": 18,
+        "gender": "Female"}  # dictionary
+overhype = None  # NoneType
+
+
+print(name, type(name))
+print(age, type(age))
+print(gender, type(gender))
+print(weight, type(weight))
+print(isStudent, type(isStudent))
+print(fruits, type(fruits))
+print(earning, type(earning))
+print(earn, type(earn))
+print(info, type(info))
+print(overhype, type(overhype))
